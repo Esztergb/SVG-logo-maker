77f2ce8015278ef7fcfbfb6979aa0fa6c3ccbadc
Eszter Barbuscia <gubiscom@yahoo.com>
2023-06-11 16:48:37 -0700

    shape classes

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
 //packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const { Triangle, Square, Circle } = require("./lib/shapes");
 
-//linking to page where the readme is developed
-const generateSVG = require("./lib/generateSVG");
+function generateSVG 
 
-//array of questions for user input using inquirer
+
 const questions = () => {
   
   return inquirer.prompt([
     {
-      type: "input",
-      message: "Please enter color code or HEX# for your logo text!",
-      name: "text-color",
-    },
-    {
       type: "list",
       message: "Pick a shape for your logo!",
-      name: "shape",
-      choices: ['circle', 'triangle', 'square']
+      name: "logoShape",
+      choices: ["circle", "triangle", "square"],
     },
     {
       type: "input",
       message: "Please enter color code or HEX# for your logo background!",
-      name: "logo-color",
+      name: "logoColor",
     },
     {
       type: "input",
-      message:
-        "Please enter a 3 character text for your logo",
-      name: "usage",
+      message: "Please enter a 1-3 character text for your logo",
+      name: "text",
     },
-]);
+    {
+      type: "input",
+      message: "Please enter color code or HEX# for your logo text!",
+      name: "textColor",
+    },
+  ]);
 };
   
 
@@ -42,7 +41,7 @@ const writeFile = (data) => {
     if (err) {
       console.log(err);
       return;
-      // when the README has been created
+      // when the logo has been created
     } else {
       console.log("Generated logo.svg!");
     }
