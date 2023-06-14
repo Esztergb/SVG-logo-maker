b42a5aec1501e9ece59d526ae2c3c6e93c93d826
Eszter Barbuscia <gubiscom@yahoo.com>
2023-06-13 19:58:52 -0700

    readme created

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
 //packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-const questions = require("./lib/questions.js");
-const fileName = "./examples/logo.svg";
-const setShape = require("./lib/setshape.js");
+const { generateSVG } = require("./lib/generateSVG");
+const { generateShape } = require("./lib/generateShape");
 
+inquirer
+  .prompt([
+    {
+      type: "list",
+      message: "Pick a shape for your logo!",
+      name: "shape",
+      choices: ["circle", "triangle", "square"],
+    },
+    {
+      type: "input",
+      message: "Please enter color code or HEX# for your logo background!",
+      name: "shapeColor",
+    },
+    {
+      type: "input",
+      message: "Please enter a 1-3 character text for your logo",
+      name: "text",
+    },
+    {
+      type: "input",
+      message: "Please enter color code or HEX# for your logo text!",
+      name: "textColor",
+    },
+  ])
+  .then((data) => {
+    const svgPath = "./examples/logo.svg";
+    const finalLogo = generateShape(data);
 
-// function to create new svg file using inquirer response and file system
-function createLogo(response) {
-    const svg = setShape(response);
-    fs.writeFile(fileName, svg, ()=> console.log('Generated logo.svg'));
-}
-
-// initialize, ask questions then createLogo using responses, catch any errors
-function init() {
-    inquirer 
-    .prompt(questions)
-    .then((response) => {
-        createLogo(response);
-        })
-    .catch(err => {
-            console.log(err)
-        });
-}
-
-init();
-
+    //Generate the svg logo here.
+    fs.writeFile(svgPath, generateSVG(finalLogo), (err) =>
+      err ? console.error(err) : console.log("Generated logo.svg")
+    );
+  })
+  .catch((err) => console.error(err));
+  
